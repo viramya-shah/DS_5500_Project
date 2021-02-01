ab010bddaf3a710e09eefdd1f0b9bab46040c5b9
viramya-shah <58053979+viramya-shah@users.noreply.github.com>
2021-01-31 22:39:03 -0500

    init

---
+++ helper/data_util.py
@@ -0,0 +1,19 @@
+import pandas as import pd
+import numpy as np
+import os
+
+
+class DataUtil:
+    def __init__(self, data_folder = "./raw_data"):
+        self.data_folder = data_folder
+
+    def read_csv(self, file_name: str = "./pilot.csv", low_memory: bool = False) -> pd.DataFrame:
+        """
+        Reads the raw data csv and returns a Pandas dataframe
+        :param: file_name: The file to be read
+        :param: low_memory: memory usage for the dataframe
+        :returns: Pandas' dataframe
+        """
+        return pd.read_csv(os.path.join(self.data_folder, file_name), low_memory = low_memory)
+
+    
