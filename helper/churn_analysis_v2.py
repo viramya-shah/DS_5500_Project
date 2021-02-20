11acc5f64c59fa54c333a5a4e1e81537eb3176bd
Juhi <juhi.paliwal32@gmail.com>
2021-02-20 12:24:12 -0500

    Changed EDA

--- helper/churn_analysis_v2.py
+++ helper/churn_analysis_v2.py
@@ -53,27 +53,163 @@ class ChurnAnalysis:
         :return: Plotly figures
         """
 
-        # pie chart to show imbalance
-        y = self.raw_data["Churn"].value_counts()
-        _layout = Layout(title='Churn')
-        _data = Pie(labels=self.raw_data['Churn'].unique(), values=y.values.tolist())
-        figure_1 = Figure(data=[_data], layout=_layout)
+        # # pie chart to show imbalance
+        # y = self.raw_data["Churn"].value_counts()
+        # _layout = Layout(title='Churn')
+        # _data = Pie(labels=self.raw_data['Churn'].unique(), values=y.values.tolist())
+        # figure_1 = Figure(data=[_data], layout=_layout)
+        #
+        # # contract vs monthly charges
+        # figure_2 = px.box(self.raw_data, x="Contract", y="MonthlyCharges")
+        #
+        # # tenure and monthly charges
+        # figure_3 = px.scatter(self.raw_data, x="tenure", y="MonthlyCharges",
+        #                       color='Churn', facet_col="Contract", facet_col_wrap=3,
+        #                       title="Churn Rate Analysis")
+        #
+        # # Todo: Add comment
+        # figure_4 = px.scatter(self.raw_data,
+        #                       x="tenure", y="MonthlyCharges",
+        #                       color="Churn", marginal_y="rug",
+        #                       marginal_x="histogram")
+        # Pie Plot
+        y = self.df["Churn"].value_counts()
+        colors = ["blue", "#d62728"]
+        fig = make_subplots(
+            rows=6, cols=2,
+            column_widths=[3.2, 3.2],
+            row_heights=[200.4, 197.4, 0.1, 200.4, 0.1, 200.4],
+            specs=[[{"type": "pie", "rowspan": 2}, {"type": "scatter"}],
+                   [None, {"type": "scatter"}],
+                   [None, None],
+                   [{"type": "histogram"}, {"type": "bar"}]
+                , [None, None], [{"type": "box"}, {"type": "box"}]],
 
-        # contract vs monthly charges
-        figure_2 = px.box(self.raw_data, x="Contract", y="MonthlyCharges")
+            subplot_titles=("Churn", "Charges", "", "Tenure", "PaymentMethod", "Paperless Billing", "Contract"))
 
-        # tenure and monthly charges
-        figure_3 = px.scatter(self.raw_data, x="tenure", y="MonthlyCharges",
-                              color='Churn', facet_col="Contract", facet_col_wrap=3,
-                              title="Churn Rate Analysis")
+        fig.add_trace(go.Pie(
+            labels=self.df['Churn'].unique(),
+            values=y.values.tolist(),
+            legendgroup="group",
+            marker=dict(colors=colors),
+            textinfo='percent+label'),
+            row=1, col=1)
 
-        # Todo: Add comment
-        figure_4 = px.scatter(self.raw_data,
-                              x="tenure", y="MonthlyCharges",
-                              color="Churn", marginal_y="rug",
-                              marginal_x="histogram")
+        # Scatter Plot
+        bins = [0, 10, 20, 30, 40, 50, 60, 70, 80]
+        labels = [0, 10, 20, 30, 40, 50, 60, 70]
+        self.df['tenure_bin'] = pd.cut(self.df['tenure'], bins, labels=labels)
 
-        return figure_1, figure_2, figure_3, figure_4
+        # Monthly Charges
+        df_MonthlyCharges = (self.df.groupby(['Churn', 'tenure_bin'])
+                             .MonthlyCharges.mean()
+                             .reset_index(name='avg_MonthlyCharges')
+
+                             )
+        data_MonthlyCharges_Yes = df_MonthlyCharges[df_MonthlyCharges.Churn == "Yes"]
+        data_MonthlyCharges_No = df_MonthlyCharges[df_MonthlyCharges.Churn == "No"]
+
+        fig.add_trace(
+            go.Scatter(x=data_MonthlyCharges_Yes["tenure_bin"], y=data_MonthlyCharges_Yes["avg_MonthlyCharges"],
+                       marker=dict(color="#d62728"), showlegend=False), row=1, col=2)
+        fig.add_trace(go.Scatter(x=data_MonthlyCharges_No["tenure_bin"], y=data_MonthlyCharges_No["avg_MonthlyCharges"],
+                                 marker=dict(color="blue"), showlegend=False), row=1, col=2)
+        fig.update_yaxes(title_text="MonthlyCharges", row=1, col=2)
+
+        # Total Charges
+        self.df['TotalCharges'] = pd.to_numeric(self.df['TotalCharges'], errors='coerce')
+        df_TotalCharges = (self.df.groupby(['Churn', 'tenure_bin'])
+                           .TotalCharges.mean()
+                           .reset_index(name='avg_TotalCharges')
+                           )
+
+        data_TotalCharges_Churn_Yes = df_TotalCharges[df_TotalCharges.Churn == "Yes"]
+        data_TotalCharges_Churn_No = df_TotalCharges[df_TotalCharges.Churn == "No"]
+
+        fig.add_trace(
+            go.Scatter(x=data_TotalCharges_Churn_Yes["tenure_bin"], y=data_TotalCharges_Churn_Yes["avg_TotalCharges"],
+                       marker=dict(color="#d62728"), showlegend=False), row=2, col=2)
+        fig.add_trace(
+            go.Scatter(x=data_TotalCharges_Churn_No["tenure_bin"], y=data_TotalCharges_Churn_No["avg_TotalCharges"],
+                       marker=dict(color="blue"), showlegend=False), row=2, col=2)
+        fig.update_xaxes(title_text="Tenure", range=[0, 80], row=2, col=2)
+        fig.update_yaxes(title_text="TotalCharges", row=2, col=2)
+
+        ## Histogram_Plot
+        data_Churn_Yes = self.df[data.Churn == "Yes"]
+        data_Churn_No = self.df[data.Churn == "No"]
+        fig.add_trace(go.Histogram(
+            x=data_Churn_Yes['tenure'],
+            xbins=dict(
+                start=0,
+                end=100,
+                size=10
+            ),
+            legendgroup="group", marker=dict(color="#d62728"), showlegend=False), row=4, col=1)
+        fig.add_trace(go.Histogram(
+            x=data_Churn_No['tenure'],
+            xbins=dict(
+                start=0,
+                end=100,
+                size=10
+            ),
+            legendgroup="group", marker=dict(color="blue"), showlegend=False), row=4, col=1)
+        fig.update_xaxes(title_text="Tenure(Bin)", range=[0, 80], row=4, col=1)
+        fig.update_yaxes(title_text="Count", row=4, col=1)
+
+        # Bar Plot
+        data_Churn_No_payment = data_Churn_No.groupby('PaymentMethod').size().sort_values(ascending=False).reset_index(
+            name='Count')
+        data_Churn_Yes_payment = data_Churn_Yes.groupby('PaymentMethod').size().sort_values(
+            ascending=False).reset_index(name='Count')
+        fig.add_trace(go.Bar(
+            x=data_Churn_No_payment['PaymentMethod'], y=data_Churn_No_payment['Count'],
+            marker=dict(color="blue"), showlegend=False), row=4, col=2)
+        fig.add_trace(go.Bar(
+            x=data_Churn_Yes_payment['PaymentMethod'], y=data_Churn_Yes_payment['Count'],
+            marker=dict(color="#d62728"), showlegend=False), row=4, col=2)
+
+        # BoxPlot
+        data_Churn_No_billing = data_Churn_No.groupby('PaperlessBilling').size().sort_values(
+            ascending=False).reset_index(name='Count_Pa')
+        data_Churn_Yes_billing = data_Churn_Yes.groupby('PaperlessBilling').size().sort_values(
+            ascending=False).reset_index(name='Count_Pa')
+        fig.add_trace(go.Bar(
+            x=data_Churn_No_billing['PaperlessBilling'], y=data_Churn_No_billing['Count_Pa'],
+            legendgroup="group", marker=dict(color="blue"), showlegend=False), row=6, col=1)
+        fig.add_trace(go.Bar(
+            x=data_Churn_Yes_billing['PaperlessBilling'], y=data_Churn_Yes_billing['Count_Pa'],
+            legendgroup="group", marker=dict(color="#d62728"), showlegend=False), row=6, col=1)
+        fig.update_yaxes(title_text="Count", row=6, col=1)
+
+        data_Churn_No_contract = data_Churn_No.groupby('Contract').size().sort_values(ascending=False).reset_index(
+            name='Count_Pa')
+        data_Churn_Yes_contract = data_Churn_Yes.groupby('Contract').size().sort_values(ascending=False).reset_index(
+            name='Count_Pa')
+
+        fig.add_trace(go.Bar(
+            x=data_Churn_No_contract['Contract'], y=data_Churn_No_contract['Count_Pa'],
+            legendgroup="group", marker=dict(color="blue"), showlegend=False), row=6, col=2)
+        fig.add_trace(go.Bar(
+            x=data_Churn_Yes_contract['Contract'], y=data_Churn_Yes_contract['Count_Pa'],
+            legendgroup="group", marker=dict(color="#d62728"), showlegend=False), row=6, col=2)
+        fig.update_yaxes(title_text="Count", row=6, col=2)
+
+        fig.update_layout(
+            autosize=False,
+            width=1000,
+            height=1000,
+            margin=dict(
+                l=50,
+                r=50,
+                b=100,
+                t=100,
+                pad=4
+            ),
+            paper_bgcolor="white",
+        )
+
+        return fig
 
     def _preprocess(self,
                     bins=5,
