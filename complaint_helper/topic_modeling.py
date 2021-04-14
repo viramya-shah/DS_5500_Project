c333f52b6b45c4e654675a11d3e296f5501df86c
Juhi <juhi.paliwal32@gmail.com>
2021-04-13 21:37:56 -0400

    Adding topics

--- complaint_helper/topic_modeling.py
+++ complaint_helper/topic_modeling.py
@@ -2,11 +2,10 @@ import pandas as pd
 import pickle
 import os
 import time
-
+import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.decomposition import NMF, LatentDirichletAllocation
-
-global tfidf_vectorizer
+from nltk.corpus import stopwords
 
 
 class TopicModeling:
@@ -39,6 +38,26 @@ class TopicModeling:
         print(f"Success ({int(time.time() - start_time) % 60}s)")
         print("*" * 89)
 
+    def show_topics(self, vectorizer, lda_model, n_words=20):
+        keywords = np.array(vectorizer.get_feature_names())
+        topic_keywords = []
+        for topic_weights in lda_model.components_:
+            top_keyword_locs = (-topic_weights).argsort()[:n_words]
+            topic_keywords.append(keywords.take(top_keyword_locs))
+        return topic_keywords
+
+    def predict_topic(self, text, vectorizer, lda_model, df_topic_keywords):
+        vect_text = vectorizer.transform(text)
+        topic_probability_scores = lda_model.transform(vect_text)
+        topic = df_topic_keywords.iloc[np.argmax(topic_probability_scores), 1:14].values.tolist()
+        infer_topic = df_topic_keywords.iloc[np.argmax(topic_probability_scores), -1]
+        return infer_topic
+
+    def apply_predict_topic(self, text, vectorizer, lda_model, df_topic_keywords):
+        text = [text]
+        topic = self.predict_topic(text, vectorizer, lda_model, df_topic_keywords)
+        return topic
+
     def _get_topic_extractor_data(self):
         """
 
@@ -51,14 +70,18 @@ class TopicModeling:
         """
 
         :param save:
-        :return: TFIDF score and labels
+        :return:
         """
         start_time = time.time()
         text_data = self._get_topic_extractor_data()
+
+        stop_words = set(stopwords.words('english'))
+        new_stopwords = ['comcast']
+        new_stopwords_list = stop_words.union(new_stopwords)
         tfidf_vectorizer = TfidfVectorizer(max_df=0.95,
                                            min_df=2,
                                            max_features=self.n_feature,
-                                           stop_words='english')
+                                           stop_words=new_stopwords_list)
         tfidf = tfidf_vectorizer.fit_transform(text_data)
 
         if save:
@@ -67,14 +90,14 @@ class TopicModeling:
 
         print(f"TFIDF Success ({int(time.time() - start_time) % 60})s")
         print("*" * 89)
-        return tfidf, tfidf_vectorizer.get_feature_names()
+        return tfidf, tfidf_vectorizer
 
     def _tf_vectorizer(self,
                        save: bool = True):
         """
 
         :param save:
-        :return: TF score and labels
+        :return:
         """
         start_time = time.time()
         text_data = self._get_topic_extractor_data()
@@ -91,7 +114,7 @@ class TopicModeling:
 
         print(f"TF Success ({int(time.time() - start_time) % 60})s")
         print("*" * 89)
-        return tf, tf_vectorizer.get_feature_names()
+        return tf, tf_vectorizer
 
     def nmf_model(self,
                   save: bool = True):
@@ -108,11 +131,21 @@ class TopicModeling:
                   alpha=.1,
                   l1_ratio=.5).fit(tfidf)
 
+        topic_keywords = self.show_topics(label, nmf)
+        # Topic - Keywords Dataframe
+        df_topic_keywords = pd.DataFrame(topic_keywords)
+        df_topic_keywords.columns = ['Word ' + str(i) for i in range(df_topic_keywords.shape[1])]
+        df_topic_keywords.index = ['Topic ' + str(i) for i in range(df_topic_keywords.shape[0])]
+        Topics = ["Service Quality", "TV/Phone Support", "Equipment Failure", "Connection Issue", "Billing Complaint",
+                  "Blocking Other Accounts", "Overcharges", "Bandwidth Issue", "Usage Issue", "Speed Issue"]
+        df_topic_keywords["Topics"] = Topics
+
         if save:
             path = os.path.join(self.model_output_path, 'nmf.pkl')
             pickle.dump(nmf, open(path, 'wb'))
 
-        return label
+        self.data['Topic'] = self.data[self.topic_extract_col].apply(self.apply_predict_topic,
+                                                                     args=(label, nmf, df_topic_keywords))
 
         print(f"NMF Trained Successfully ({int(time.time() - start_time) % 60}s)")
         print("*" * 89)
@@ -131,13 +164,24 @@ class TopicModeling:
                                         max_iter=5,
                                         learning_method='online',
                                         learning_offset=50.,
-                                        random_state=0).fit(tf)
+                                        random_state=0)
+        lda_fit = lda.fit(tf)
+
+        topic_keywords = self.show_topics(label_tf, lda)
+        # Topic - Keywords Dataframe
+        df_topic_keywords = pd.DataFrame(topic_keywords)
+        df_topic_keywords.columns = ['Word ' + str(i) for i in range(df_topic_keywords.shape[1])]
+        df_topic_keywords.index = ['Topic ' + str(i) for i in range(df_topic_keywords.shape[0])]
+        Topics = ["Service Quality", "TV/Phone Support", "Equipment Failure", "Connection Issue", "Billing Complaint",
+                  "Blocking Other Accounts", "Overcharges", "Bandwidth Issue", "Usage Issue", "Speed Issue"]
+        df_topic_keywords["Topics"] = Topics
 
         if save:
             path = os.path.join(self.model_output_path, 'lda.pkl')
-            pickle.dump(lda, open(path, 'wb'))
+            pickle.dump(lda_fit, open(path, 'wb'))
 
-        return label_tf
+        self.data['Topic'] = self.data[self.topic_extract_col].apply(self.apply_predict_topic,
+                                                                     args=(label_tf, lda, df_topic_keywords))
 
         print(f"LDA Trained Successfully ({int(time.time() - start_time) % 60}s)")
         print("*" * 89)
